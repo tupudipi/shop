244c95b32f4b8436c7eeb15122b635ce444058f5
tupudipi <tupudipi@gmail.com>
2024-05-15 14:27:21 +0300

    chore: Update product page layout and add recommended products section

--- src/app/products/[id]/page.js
+++ src/app/products/[id]/page.js
@@ -1,14 +1,61 @@
 import Navbar from "@/app/components/Navbar"
+import ProductCard from "@/app/components/ProductCard";
 
 const productPage = ({ params }) => {
     const productId = params.id;
     return (
         <>
             <Navbar />
-            <main className="flex min-h-screen flex-col items-center">
-                {/* product image gallery */}
-                
-                <h1 className="text-4xl font-bold">Product {productId}</h1>
+            <main className="container mx-auto px-4 md:px-8">
+                <div id="product" className="flex flex-col md:grid md:grid-cols-2 gap-4 bg-white/30 border shadow rounded-lg p-8">
+                    <div className="grid place-content-center">
+                        <div className="w-80 h-80 bg-gray-200 rounded-lg"></div>
+                    </div>
+
+                    <div className="max-w-2xl flex flex-col justify-between">
+                        <div>
+                            <h1 className="text-3xl mb-2">Product {productId}</h1>
+                            <div className="flex gap-4 align-middle">
+                                <div className="flex gap-1 align-middle">
+                                    <div className="w-4 h-4 bg-yellow-500 rounded-full"></div>
+                                    <div className="w-4 h-4 bg-yellow-500 rounded-full"></div>
+                                    <div className="w-4 h-4 bg-yellow-500 rounded-full"></div>
+                                    <div className="w-4 h-4 bg-yellow-500 rounded-full"></div>
+                                    <div className="w-4 h-4 bg-gray-300 rounded-full"></div>
+                                </div>
+                                <p className="text-sm">4.0 <span className="text-gray-500">(98)</span></p>
+                            </div>
+                        </div>
+
+                        <p className="font-bold text-2xl">$10.00</p>
+
+                        <div>
+                            <div className="flex flex-col mt-4 gap-4">
+                                <div className="flex flex-col gap-2">
+                                    <p className="text-green-700">In stock</p>
+
+                                    <div className="md:w-3/4 lg:w-2/3 flex flex-col gap-2">
+                                        <div className="flex gap-2">
+                                            <button className="bg-gray-200 text-gray-800 px-4 py-1 rounded-lg hover:bg-gray-300 transition-colors">-</button>
+                                            <input className="flex-grow min-w-0 text-center" type="number" value="1" />
+                                            <button className="bg-gray-200 text-gray-800 px-4 py-1 rounded-lg hover:bg-gray-300 transition-colors">+</button>
+                                        </div>
+                                        <button className="bg-blue-500 text-white px-4 py-1 rounded-lg hover:bg-blue-700 transition-colors">Add to Cart</button>
+                                        <button className="bg-gray-200 text-gray-800 px-4 py-1 rounded-lg hover:bg-gray-300 transition-colors">Add to favourites</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div id="recommended" className="flex gap-4 p-8 mt-4">
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                </div>
             </main>
         </>
     )
