e51e6d0ce828943ad0d0ff7e998611f6b170572f
tupudipi <tupudipi@gmail.com>
2024-06-21 13:46:31 +0300

    updated account page layout file to handle auth status. removed unused register page

--- src/app/account/layout.js
+++ src/app/account/layout.js
@@ -1,5 +1,7 @@
 import Navbar from "../components/Navbar";
 import AccountSidebar from "../components/AccountSidebar";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
 
 export const metadata = {
     title: 'Account - Cico Shop',
@@ -7,13 +9,25 @@ export const metadata = {
     keywords: 'cico shop, account'
 }
 
-export default function RootLayout({ children }) {
+export default async function RootLayout({ children }) {
+    const session = await getServerSession(authOptions);
+
     return (
         <>
             <Navbar />
             <main className="container mx-auto flex flex-col md:flex-row px-4 pb-4 gap-4 md:gap-8 lg:gap-24">
-                <AccountSidebar />
-                {children}
+                {
+                    session ? (
+                        <>
+                            <AccountSidebar />
+                            {children}
+                        </>
+                    ) : (
+                        <div className="container">
+                            <h1 className="text-xl font-medium">You must be logged in to access your account.</h1>
+                        </div>
+                    )
+                }
             </main>
         </>
     )
